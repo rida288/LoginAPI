c7bf99ad333677e6fd3400e2cb09b5dda6a4013f
rida288 <fatima166rida@gmail.com>
2026-08-13 15:39:39 +0500

    fix db url

--- app/core/database.py
+++ app/core/database.py
@@ -1,21 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:pwd@127.0.0.1:5433/postgres"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:pwd@127.0.0.1:5433/postgres"  # local fallback for dev
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL) 
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base() #create table w/class 
+Base = declarative_base()
 
-#allow to get an instance of a session to interact w/db
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
- 
